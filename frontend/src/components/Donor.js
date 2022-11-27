3c87fbda7a1d5e655481c3d02b7cb18c90ef270a
aryan <aryanpatil1503@gmail.com>
2022-11-27 12:05:48 +0530

    final commit

---
+++ frontend/src/components/Donor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DonorDashBoard from './views/DonorView/DonorDashBoard'
+const Donor = () => {
+    return (
+        <div>
+            <DonorDashBoard/>
+        </div>
+    )
+}
+
+export default Donor
---
+++ frontend/src/components/Patient.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PatientDashBoard from './views/PatientView/PatientDashBoard'
+const Patient = () => {
+    return (
+        <div>
+            <PatientDashBoard/>
+        </div>
+    )
+}
+
+export default Patient
